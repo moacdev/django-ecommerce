df66b06c38e904d9daa668e0459e156050ceb2fe
moacdev <moacdev@gmail.com>
2022-03-20 12:30:20 +0000

    tiny changes

--- mkplace/urls.py
+++ mkplace/urls.py
@@ -17,27 +17,8 @@ from django.contrib import admin
 from django.shortcuts import get_object_or_404, render
 from django.urls import path
 from store.models import Categorie, Product
-from store.views import homepage
+from store.views import categories, category, homepage, login, product, register
 
-def login(request):
-    return render(request, "store/login.html",{ "categories": Categorie.objects.all() })
-
-def register(request):
-    return render(request, "store/register.html",{ "categories": Categorie.objects.all() })
-
-def categories(request):
-    categories = Categorie.objects.all()
-    return render(request, "store/categories.html", { "categories": Categorie.objects.all() })
-
-def category(request, category):
-    _category = get_object_or_404(Categorie, slug=category)
-    return render(request, "store/category.html", { "categories": Categorie.objects.all(), "category": _category, "products": Product.objects.filter(category=_category.id) })
-
-def product(request, category, product):
-    print(category)
-    _category = get_object_or_404(Categorie, slug=category)
-    _product = get_object_or_404(Product, slug=product, category=_category.id)
-    return render(request, "store/view_product.html", { "categories": Categorie.objects.all(), "category": _category, "product": _product })
 
 urlpatterns = [
     path('', homepage),
