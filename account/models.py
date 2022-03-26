531261427b56815d865e281bd03d495cb2a36301
moacdev <moacdev@gmail.com>
2022-03-26 17:01:30 +0000

    address finished

--- account/models.py
+++ account/models.py
@@ -4,10 +4,23 @@ from base.settings import AUTH_USER_MODEL
 
 from store.models import Order
 
+
+
 class Shopper(AbstractUser):
     phone = CharField(unique=True, null=True, max_length=15)
     def __str__(self) -> str:
         return super().username
+
+class Address(Model):
+    country = CharField('Pays', max_length=128, default="Mali", blank=True)
+    city = CharField('Ville', max_length=128, blank=True)
+    common = CharField('Commune', max_length=128, blank=True)
+    quarter = CharField('Quartier', max_length=128, blank=True)
+    street_door = CharField('Rue et porte', max_length=128, blank=True)
+    zipcode = CharField('Code postal', max_length=128, blank=True)
+    user = OneToOneField(AUTH_USER_MODEL, on_delete=CASCADE)
+    def __str__(self) -> str:
+        return f"Adresse de {self.user}"
 
 class Cart(Model):
     user = ForeignKey(AUTH_USER_MODEL, on_delete=CASCADE)
--- base/urls.py
+++ base/urls.py
@@ -14,26 +14,93 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import path
+from account.models import Address
 from store.models import Categorie, Product
 from store.views import categories, category, homepage, login, product, register
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
 
 
 def accountInfo(request):
-    return render(request, "store/account.html", { "categories": Categorie.objects.all() })
+    if request.user.is_authenticated != True:
+        return redirect(to="/connexion")
+    
+    user = request.user
+    address = Address.objects.get(user=user)
+
+    print(address)
+
+    return render(request, "store/account.html", { "categories": Categorie.objects.all(), 'user': request.user, 'address': address })
 def changePwd(request):
+    if request.user.is_authenticated != True:
+        return redirect(to="/connexion")
     return render(request, "store/change-password.html", { "categories": Categorie.objects.all() })
 def wishList(request):
+    if request.user.is_authenticated != True:
+        return redirect(to="/connexion")
     return render(request, "store/wishlist.html", { "categories": Categorie.objects.all() })
 
 def cart(request):
+    if request.user.is_authenticated != True:
+        return redirect(to="/connexion")
     return render(request, "store/cart.html", { "categories": Categorie.objects.all() })
 def orders(request):
+    if request.user.is_authenticated != True:
+        return redirect(to="/connexion")
     return render(request, "store/orders.html", { "categories": Categorie.objects.all() })
 def search(request):
     return render(request, "store/search.html", { "categories": Categorie.objects.all() })
 def helpFAQ(request):
+    return render(request, "store/faq.html", { "categories": Categorie.objects.all() })
+
+
+def apiChangePassword(request):
+    # On accepte que les requêtes en postes et l'utilisateur doit aussi être authentifié authentifié
+    if request.method != 'POST' and request.user.is_authenticated != True:
+        raise PermissionDenied()
+    # On récupere les donnée envoyée
+    current_password = request.POST.get('password')
+    new_password = request.POST.get('new_password')
+    password_confirm = request.POST.get('password_confirmation')
+    # On vérifie si les donnée sont valide
+    if not current_password or not new_password or not password_confirm or (new_password != password_confirm):
+        return JsonResponse({ "isOk": False,  "error_type": "fields-invalid"  })
+    # On vérifie si le mot de pass actuelle est valide
+    if not request.user.check_password(current_password):
+            return JsonResponse({ "isOk": False,  "error_type": "password-invalid"  })
+    # On met à jour le mot de passe du user
+    request.user.set_password(new_password)
+    request.user.save()
+    # On return à la vue une reponse comme quoi tous ses bien passée
+    return JsonResponse({ "isOk": True })
+
+
+def apiChangeAddresses(request):
+    # On accepte que les requêtes en postes et l'utilisateur doit aussi être authentifié authentifié
+    if request.method != 'POST' and request.user.is_authenticated != True:
+        raise PermissionDenied()
+    # On récupere les donnée envoyée
+    country = request.POST.get('country')
+    city = request.POST.get('city')
+    common = request.POST.get('common')
+    quarter = request.POST.get('quarter')
+    streetDoor = request.POST.get('streetDoor')
+    zipcode = request.POST.get('zipcode')
+    user = request.user
+    try:
+        Address.objects.update_or_create(user=user, defaults={"country":country, "city":city, "common":common, "quarter":quarter, "street_door":streetDoor, "zipcode":zipcode, "user": user} )
+    except:
+        return JsonResponse({ "isOk": False })
+
+    # On return à la vue une reponse comme quoi tous ses bien passée
+    return JsonResponse({ "isOk": True })
+
+def apiChangeProfilInfo(request):
+    return render(request, "store/faq.html", { "categories": Categorie.objects.all() })
+def apiChangePaymentInfo(request):
     return render(request, "store/faq.html", { "categories": Categorie.objects.all() })
 
 
@@ -52,6 +119,11 @@ urlpatterns = [
     path('mes-commandes', orders),
     path('recherche/', search),
     path('faq/', helpFAQ),
+
+    path('api/account/change-password', apiChangePassword),
+    path('api/account/change-addresses-info', apiChangeAddresses),
+    path('api/account/change-profil-info', apiChangeProfilInfo),
+    path('api/account/change-payment-info', apiChangePaymentInfo),
 
 
 
